3654f23bd3460a424efa7efe3caaecf8da027a62
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2014-06-13 15:05:56 +0200

    GUI tkinter/toolbar: add fake get/set functions (WiP)

--- energy_meter_setup.py
+++ energy_meter_setup.py
@@ -5,11 +5,23 @@ from Tkinter import *
 root = Tk()
 root.title("setup wavelength")
 
+def set_wavelength(value):
+    print "set "+str(value)+" nm."
+
+def get_wavelength():
+    value=1234
+    print "get "+str(value)+" nm."
+    return value;
+
 def callback1064():
     print "called the callback 1064"
+    set_wavelength(1064)
+    print 'current='+str(get_wavelength())
 
 def callback532():
     print "called the callback 532"
+    set_wavelength(532)
+    print 'current='+str(get_wavelength())
 
 # create a toolbar
 toolbar = Frame(root)
