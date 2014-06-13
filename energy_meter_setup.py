35b487ff5b430407da3f975c1eeaa7a0404671bd
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2014-06-13 15:01:03 +0200

    GUI tkinter/toolbar: 2 buttons for wavelength 1064 and 512 (WiP)

--- energy_meter_setup.py
+++ energy_meter_setup.py
@@ -3,20 +3,26 @@
 from Tkinter import *
 
 root = Tk()
+root.title("setup wavelength")
 
-def callback():
-    print "called the callback!"
+def callback1064():
+    print "called the callback 1064"
+
+def callback532():
+    print "called the callback 532"
 
 # create a toolbar
 toolbar = Frame(root)
 
-b = Button(toolbar, text="new", width=6, command=callback)
+b = Button(toolbar, text="1064 nm", width=6, command=callback1064)
 b.pack(side=LEFT, padx=2, pady=2)
 
-b = Button(toolbar, text="open", width=6, command=callback)
+b = Button(toolbar, text="532 nm", width=6, command=callback532)
 b.pack(side=LEFT, padx=2, pady=2)
 
 toolbar.pack(side=TOP, fill=X)
 
 root.mainloop()
+
+#Example : *PWC01064 selects the wavelength 1064 nm
 
